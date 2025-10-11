c85963837c83b88bea5fdc4b99a1801614ed536d
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2025-10-11 16:26:56 +0300

    Initial commit - PHP Docker setup

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!<br>";
+?>
+
+<?php
+echo "Hello from PHP in Docker!<br>";
+echo "<i>Hello from PHP in Docker - this is changed line!</i><br>";
+echo "PHP version: " . phpversion();
+?>
+
+</body>
+</html>
